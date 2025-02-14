585622228299f5318f385091be5de8f053a1fff4
Mahmut <miftarosky@hotmail.com>
2025-02-14 04:39:03 +0100

    refactor: remove files for restructuring

--- config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    TOKEN: process.env.DISCORD_BOT_TOKEN,
-    SOURCE_CHANNEL_ID: process.env.SOURCE_CHANNEL_ID,
-    TARGET_CHANNEL_ID: process.env.TARGET_CHANNEL_ID,
-    PREFIX: process.env.PREFIX || '!',
-}; 
